3efcc87fbb0080224570a4095cdddf6d2c8a6cb5
Matt Smith <mss@datera.io>
2016-06-10 09:32:46 -0700

    Adding additional test and coverage report to Travis CI

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,12 +1,19 @@
 import pytest
+from mock import MagicMock as Mock
 
 
-def test_import():
+def test_api_import():
 
     from dfs_sdk import DateraApi
 
 
-def test_failed_import():
+def test_api_import_failed():
 
     with pytest.raises(ImportError):
         from dfs_sdk import NotDateraApi
+
+def test_types_import():
+
+    from dfs_sdk import Auth
+    auth = Auth(Mock(), Mock({}))
+    assert auth is not None
